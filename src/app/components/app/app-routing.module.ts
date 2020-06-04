4667bbb73b1d1d16252e73fd0ad81fb657957a79
Yankee1234 <vovandemort2013@gmail.com>
2020-06-04 12:14:52 +0300

    AddedLogin/UpdatedArchitecture

--- src/app/components/app/app-routing.module.ts
+++ src/app/components/app/app-routing.module.ts
@@ -1,18 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from '../home/home.component';
+import { LoginComponent } from '../login/login.component';
 
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: "full"},
   {path:'home' , component: HomeComponent},
-  {path:'login'},
-  {path:'register'},
+  {path:'login', component: LoginComponent},
+  /*{path:'register'},
   {path:'store'},
   {path:'rating'},
   {path:'about'},
   {path:'profile'},
-  {path:'forum'},
+  {path:'forum'},*/
 ];
 
 @NgModule({
--- src/app/components/app/app.module.ts
+++ src/app/components/app/app.module.ts
@@ -2,17 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app/app.component';
+import { AppComponent } from './app.component';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { ReactiveFormsModule } from '@angular/forms';
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from '../home/home.component';
+import { LoginComponent } from '../login/login.component';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
